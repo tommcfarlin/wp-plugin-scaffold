da426b196cbb922f9de8118b7a286f10f64d3b08
Tom McFarlin <tom@tommcfarlin.com>
2022-01-21 11:05:08 -0500

    Add plugin bootstrap

---
+++ plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WP Plugin Scaffold
+ *
+ * TODO: The description for the plugin scaffolded by this file.
+ *
+ * PHP version 7.4.27
+ *
+ * @category WordPress_Plugin
+ * @package  TODO
+ * @author   Tom McFarlin <tom@tommcfarlin.com>
+ * @license  GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ * @link     https://github.com/tommcfarlin/wp-plugin-scaffold/
+ * @since    TODO: Date
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP Plugin Scaffold
+ * Plugin URI:  https://github.com/tommcfarlin/wp-plugin-scaffold/
+ * Description: The description for the plugin scaffolded by this file.
+ * Author:      Tom McFarlin <tom@tommcfarlin.com>
+ * Version:     1.0.0
+ */
+
+namespace WPPluginScaffold;
+
+defined('WPINC') || die;
+require_once __DIR__ . '/vendor/autoload.php';
